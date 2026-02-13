01a3f5bcaf5e18f2cd6b0a0a65dfba67cea34fa6
akoncore <akniet.talgat.05@mail.ru>
2026-02-13 13:22:04 +0500

    fixed:settings

--- settings/conf.py
+++ settings/conf.py
@@ -1,5 +1,5 @@
 from decouple import config
-from datetime import timedelta
+
 
 
 #-----------------------------
@@ -12,28 +12,3 @@ ENV_POSSIBLE_OPTIONS = (
 ENV_ID = config("BLOG_ENV_ID",cast=str)
 SECRET_KEY = 'django-insecure-+ada5^xvx&doe5tpzsdnlu6qb%llv6*^#4l@rh@tz((5*!u!vw'
 
-
-#------------------------
-#REST 
-#
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
-
-
-#---------------------------------
-#Simple-JWT
-#
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
-    "AUTH_HEADER_TYPES": ("Bearer",),
-    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
-}
